842323b0750b7f7af9d06c15cd0172ccf72101e4
amargh2 <margherioa@gmail.com>
2022-11-05 19:34:47 -0500

    lat and long register on map when clicked, working on passing to weather api call

--- components/testmap.tsx
+++ components/testmap.tsx
@@ -1,17 +1,21 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import useSWR from 'swr'
 
 // Please be a decent human and don't abuse my Mapbox API token.
 // If you fork this sandbox, replace my API token with your own.
 // Ways to set Mapbox token: https://uber.github.io/react-map-gl/#/Documentation/getting-started/about-mapbox-tokens
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
+
 export default function TestMap () {
   const mapContainer = useRef(null);
   const map = useRef(null);
   const [lng, setLng] = useState(-70.9);
   const [lat, setLat] = useState(42.35);
   const [zoom, setZoom] = useState(9);
-  
+  const fetcher = (...args) => fetch(...args).then(res => res.json())
+
+
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
@@ -30,7 +34,14 @@ export default function TestMap () {
     setZoom(map.current.getZoom().toFixed(2));
     });
     });
-
+  
+  useEffect(() => {
+    if (!map.current) return;
+    map.current.on('click', (e) => {
+      console.log(e.lngLat.lng, e.lngLat.lat)
+    })
+  })
+  //const {data, error} = useSWR(`https://api.openweathermap.org/data/3.0/onecall?lat=${e.lat}&lon=${e.lon}&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`)
 
   return (
     <div className='grid'>
@@ -38,6 +49,7 @@ export default function TestMap () {
         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
       </div>
       <div ref={mapContainer} className='map-container'></div>
+      
     </div>
   )
 
