8d73b50772a2f80e712eac8368d946335acd0b8c
amargh2 <margherioa@gmail.com>
2022-11-06 13:26:42 -0600

    weather data populates when geocoder result fires

--- components/testmap.tsx
+++ components/testmap.tsx
@@ -1,19 +1,21 @@
 import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import useSWR from 'swr'
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { on } from 'process';
+import { FaDatabase } from 'react-icons/fa';
 
-// Please be a decent human and don't abuse my Mapbox API token.
-// If you fork this sandbox, replace my API token with your own.
-// Ways to set Mapbox token: https://uber.github.io/react-map-gl/#/Documentation/getting-started/about-mapbox-tokens
-mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
+//added or statement to stop ts possibly undefined error
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN || 'iejreijreij';
 
+//instantiating the geocoder
 const geocoder = new MapboxGeocoder({
   accessToken: mapboxgl.accessToken,
   mapboxgl: mapboxgl
 });
+
+
 
 export default function TestMap () {
   const mapContainer = useRef(null);
@@ -21,10 +23,12 @@ export default function TestMap () {
   const [lng, setLng] = useState(30);
   const [lat, setLat] = useState(50);
   const [zoom, setZoom] = useState(1.5);
+  const [weather, setWeather] = useState('')
+  
   const fetcher = (...args) => fetch(...args).then(res => res.json())
   //adding  user selected clicked coordinates *this might end up getting lifted up*
-  const [clickedLng, setClickedLng] = useState(-70.9)
-  const [clickedLat, setClickedLat] = useState(42.35);
+  const [searchedLng, setSearchedLat] = useState(-70.9)
+  const [searchedLat, setSearchedLng] = useState(42.35);
   useEffect(() => {
     if (map.current) return; // initialize map only once
     map.current = new mapboxgl.Map({
@@ -34,38 +38,26 @@ export default function TestMap () {
     zoom: zoom,
     projection:'globe'
     });
-    map?.current?.addControl(
+    map.current.addControl(
       geocoder
-        .on('result', (e) => (console.log(e.result.geometry.coordinates)))
+        .on('result', (e) => {
+          (console.log(e.result.geometry.coordinates.at(0)))
+          setSearchedLat(e.result.geometry.coordinates.at(0))
+          setSearchedLng(e.result.geometry.coordinates.at(1))
+          setWeather(searchedLat, searchedLng)
+        })
     )
   });
 
-  useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
-    map?.current?.on('move', () => {
-    setLng(map?.current?.getCenter().lng.toFixed(4));
-    setLat(map?.current?.getCenter().lat.toFixed(4));
-    setZoom(map?.current?.getZoom().toFixed(2));
-    });
-    });
-  
-  useEffect(() => {
-    if (!map.current) return;
-    map.current.on('click', (event) => {
-      setClickedLng(event.lngLat.lng)
-      setClickedLat(event.lngLat.lat)
-      console.log(clickedLat, clickedLng)
-    })
-  })
  
-  //const {data, error} = useSWR(`https://api.openweathermap.org/data/3.0/onecall?lat=${clickedLat}&lon=${clickedLng}&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`, fetcher)
-  //console.log(data)
+  const {data, error} = useSWR(`https://api.openweathermap.org/data/3.0/onecall?lat=${searchedLat}&lon=${searchedLng}&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}`, fetcher)
+  if (data) console.log(JSON.stringify(data.daily))
   return (
     <div className='mapbox'>
-      <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div>
       <div ref={mapContainer} className='map-container'></div>
+      <div>
+        {data? JSON.stringify(data.daily) : ''}
+      </div>
     </div>
   )
 
